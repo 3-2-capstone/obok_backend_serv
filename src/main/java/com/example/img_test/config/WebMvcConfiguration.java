95c0b1b6b7aba27a5ae1377a9dcc497518ed328a
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-11-14 20:39:09 +0900

    change data

--- src/main/java/com/example/img_test/config/WebMvcConfiguration.java
+++ src/main/java/com/example/img_test/config/WebMvcConfiguration.java
@@ -9,6 +9,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:///home/ubuntu/img_test/");
+        registry.addResourceHandler("/**").addResourceLocations("file:///home/ubuntu/img_test/build/libs/");
     }
 }
